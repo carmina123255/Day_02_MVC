fc040f55543ca5e4c3cadc1a1c065da23f014118
carmi <carmi@CERMINA_MAGED>
2025-02-22 16:05:53 +0200

    Action ReturnTypes

--- Demo/Controllers/MoviesController.cs
+++ Demo/Controllers/MoviesController.cs
@@ -5,27 +5,78 @@ namespace Demo.Controllers
 {
     public class MoviesController:Controller
     {
+        private readonly IConfiguration _configuration;
+
         //Action ==>Public nonstatic Object Member Method inside the Controller 
-        [HttpGet] //baseUrl/Movies/Index
-        [ActionName("Index")]
-        public string GetAllMovie()
+
+        #region Routing 
+        /// [HttpGet] //baseUrl/Movies/Index
+        /// [ActionName("Index")]
+        /// public string GetAllMovie()
+        /// {
+        ///     return "All Movies";
+        /// }
+        /// [HttpGet] //baseUrl/Movies/GetMovie/{id}?name=Avatar
+        /// public string GetMovie(int id,string name )
+        /// {
+        ///     return $"Movie with id : {id} And Name :{name}";
+        /// }
+        ///
+        /// public ViewResult CreateMovie()
+        /// {
+        ///     return View();  
+        /// }
+        /// [ActionName("ConfirmCreateMovie")]
+        /// public OkResult CreateMovie(Movie movie)
+        /// {
+        ///     return Ok();
+        /// }
+        ///  
+        #endregion
+
+        public MoviesController(IConfiguration configuration)
         {
-            return "All Movies";
+            _configuration = configuration;
         }
-        [HttpGet] //baseUrl/Movies/GetMovie/{id}?name=Avatar
-        public string GetMovie(int id,string name )
+        
+        public IActionResult GetMovie(int id)
+
         {
-            return $"Movie with id : {id} And Name :{name}";
+            if (id == 0) return BadRequest();             // return new BadRequestResult();
+            else if (id == 100) return NotFound();        //return new NotFoundResult();
+            else
+            {
+                /// ContentResult result = new ContentResult();
+                /// result.Content = $"Movie with id : {id}";
+                /// result.ContentType = "text/html";
+                /// result.StatusCode = 200;
+                /// 
+                /// return result;
+                /// 
+                return Content($"Movie with id : {id}", "text/html");
+            }
+
         }
 
-        public ViewResult CreateMovie()
+        public IActionResult Kermina()
         {
-            return View();  
-        }
-        [ActionName("ConfirmCreateMovie")]
-        public OkResult CreateMovie(Movie movie)
-        {
-            return Ok();
+          /// RedirectResult result = new RedirectResult("https://wuzzuf.net/jobs/egypt");
+          /// RedirectResult result = new RedirectResult(_configuration["WuzzufUrl"]??string.Empty);
+          /// RedirectResult result = new RedirectResult($"{_configuration["MovieUrl"]}/Movies/GetMovie/10");
+          /// return result;
+
+
+            /// RedirectToActionResult result = new RedirectToActionResult(nameof(GetMovie),"Movies", new {id=10});
+            ///      return result;
+
+
+
+           /// RedirectToRouteResult result = new RedirectToRouteResult("default", new { Controller = "Movies", action = "GetMovie", id = 10 });
+           ///
+           /// return result;
+
+
+            return Redirect("https://wuzzuf.net/jobs/egypt");
         }
     }
 }
