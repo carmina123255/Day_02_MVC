c8354154c2bf078a5fe53563ed4061e67fbdf6b6
carmi <carmi@CERMINA_MAGED>
2025-02-22 02:32:35 +0200

    Important Revision

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -1,15 +1,46 @@
-namespace Demo
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
-            var app = builder.Build();
 
-            app.MapGet("/", () => "Hello World!");
+            var webApplicationBuilder = WebApplication.CreateBuilder();
 
-            app.Run();
-        }
-    }
-}
+            #region Configure Services 
+            webApplicationBuilder.Services.AddControllersWithViews();
+            #endregion
+
+            var App = webApplicationBuilder.Build();
+
+            #region Configure 
+
+
+            if (App.Environment.IsDevelopment())
+            {
+                App.UseDeveloperExceptionPage();//MiddleWAre
+            }
+            else
+            {
+                App.UseStatusCodePagesWithReExecute("/Home/Error");
+            }
+            App.UseRouting();
+
+            App.MapGet("/", async context =>
+            {
+                await context.Response.WriteAsync("Hello world!");
+            });
+
+            App.MapPost("/Hamada", async context =>
+            {
+                await context.Response.WriteAsync("Hello Hamada!");
+            });
+
+
+            App.MapControllerRoute(
+                name: "default",
+                pattern: "{Controller}/{action}/{id?}"
+
+                );
+            #endregion
+
+            App.Run();
+
+
+
+
+ 
